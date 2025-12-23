3d72ef1c5a90753bc119e31a5a81828b4c0ceeb6
sssuhha <ysh83@sookmyung.ac.kr>
2025-12-23 23:29:36 +0900

    feat: 디자인 시스템 세팅

--- app/src/main/java/com/solux/moro/core/designsystem/theme/Theme.kt
+++ app/src/main/java/com/solux/moro/core/designsystem/theme/Theme.kt
@@ -1,57 +1,64 @@
 package com.solux.moro.core.designsystem.theme
 
-import android.os.Build
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+val localColors = staticCompositionLocalOf { defaultColors }
+val localTypography = staticCompositionLocalOf { defaultTypography }
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+object MoroTheme {
+    val colors: Colors
+        @Composable
+        @ReadOnlyComposable
+        get() = localColors.current
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+    val typography: Typography
+        @Composable
+        @ReadOnlyComposable
+        get() = localTypography.current
+}
 
 @Composable
-fun MorofrontendTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideMoroColorsAndTypography(
+    colors: Colors,
+    typography: Typography,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    CompositionLocalProvider(
+        localColors provides colors,
+        localTypography provides typography,
         content = content
     )
+}
+
+@Composable
+fun MoroTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    ProvideMoroColorsAndTypography(
+        colors = defaultColors,
+        typography = defaultTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars =
+                        !darkTheme
+                }
+            }
+        }
+        MaterialTheme(
+            content = content
+        )
+    }
 }
