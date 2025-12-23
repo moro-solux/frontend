c4e96cbc5acdd47c7a194263293926aad05e704a
sssuhha <ysh83@sookmyung.ac.kr>
2025-12-23 21:06:57 +0900

    chore: 불필요한 util 함수 삭제

--- app/src/main/java/com/solux/moro/core/util/IntExt.kt
+++
@@ -1,3 +0,0 @@
-package com.solux.moro.core.util
-
-fun Int.toStringWithFormat(): String = java.text.DecimalFormat("#,###").format(this)
--- app/src/main/java/com/solux/moro/core/util/ModifierExt.kt
+++ app/src/main/java/com/solux/moro/core/util/ModifierExt.kt
@@ -27,48 +27,5 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
         }
     }
 
-fun Modifier.dropShadow(
-    shape: Shape,
-    color: Color = Color.Black.copy(0.25f),
-    blur: Dp = 4.dp,
-    offsetY: Dp = 4.dp,
-    offsetX: Dp = 0.dp,
-    spread: Dp = 0.dp
-) = this.drawBehind {
-    val shadowSize = Size(size.width + spread.toPx(), size.height + spread.toPx())
-    val shadowOutline = shape.createOutline(shadowSize, layoutDirection, this)
 
-    val paint = Paint().apply {
-        this.color = color
-    }
 
-    if (blur.toPx() > 0) {
-        paint.asFrameworkPaint().apply {
-            maskFilter = BlurMaskFilter(blur.toPx(), BlurMaskFilter.Blur.NORMAL)
-        }
-    }
-
-    drawIntoCanvas { canvas ->
-        canvas.save()
-        canvas.translate(offsetX.toPx(), offsetY.toPx())
-        canvas.drawOutline(shadowOutline, paint)
-        canvas.restore()
-    }
-}
-
-fun Modifier.bottomBorder(
-    strokeWidth: Dp,
-    color: Color
-): Modifier =
-    this.drawBehind {
-        val strokeWidthPx = strokeWidth.toPx()
-        val width = size.width
-        val height = size.height - strokeWidthPx / 2
-
-        drawLine(
-            color = color,
-            start = Offset(x = 0f, y = height),
-            end = Offset(x = width, y = height),
-            strokeWidth = strokeWidthPx
-        )
-    }
