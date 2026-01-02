f1593f1102d76d09fe9cb55e15446ce4246e77cd
leece03 <164038699+leece03@users.noreply.github.com>
2026-01-03 04:09:58 +0900

    feat: 프로필 편집 화면 생성

--- app/src/main/java/com/solux/moro/ui/profile/component/profile.kt
+++ app/src/main/java/com/solux/moro/ui/profile/component/profile.kt
@@ -191,7 +191,7 @@ fun Profile(){
                         color = Color(0xFFFFFFFF),
                         ),
                     modifier = Modifier
-                        .width(94.toPxDp)
+                        //.width(94.toPxDp)
                         .height(48.toPxDp)
                 )
             }
@@ -209,6 +209,139 @@ fun Profile(){
         }
     }
 }
+
+@Composable
+fun ProfileHeader(){
+    Column(
+        Modifier
+            .width(1080.toPxDp)
+            .padding(start = 46.08.toPxDp, top = 46.08.toPxDp, end = 46.08.toPxDp, bottom = 46.08.toPxDp),
+        verticalArrangement = Arrangement.spacedBy(57.60000228881836.toPxDp, Alignment.Top),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .width(457.92001.toPxDp)
+                .padding(start = 8.64.toPxDp, end = 8.64.toPxDp),
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .width(457.92001.toPxDp)
+                    .height(460.79999.toPxDp)
+                    .background(
+                        brush = Brush.radialGradient(
+                            colors = listOf(Color(0xFF3366FF), Color(0x003D9900)),
+                            center = Offset.Unspecified, // 기본값은 중앙
+                            radius = Float.POSITIVE_INFINITY // 기본값은 컴포넌트 크기에 맞춤
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .width(276.48001.toPxDp)
+                    .height(276.48001.toPxDp)
+            ) {
+                Image(
+                    modifier = Modifier
+                        .width(276.48001.toPxDp)
+                        .height(276.48001.toPxDp)
+                        .clip(RoundedCornerShape(size = 28797.12109.toPxDp))
+                        .border(
+                            width = 11.52.toPxDp,
+                            color = Color(0xFF1F2937),
+                            shape = RoundedCornerShape(size = 28797.12109.toPxDp)
+                        )
+                        .padding(0.31706.toPxDp)
+                        .background(Color.White),
+                    painter = painterResource(id = R.drawable.img_profile_large),
+                    contentDescription = "image description",
+                    contentScale = ContentScale.FillHeight,
+                )
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(
+                        0.dp,
+                        Alignment.CenterHorizontally
+                    ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .offset(x = 207.35938.toPxDp, y = 207.35938.toPxDp)
+                        .border(
+                            width = 5.76.toPxDp,
+                            color = Color(0xFFFFFFFF),
+                            shape = RoundedCornerShape(size = 28797.12109.toPxDp)
+                        )
+                        .width(69.12.toPxDp)
+                        .height(69.12.toPxDp)
+                        .background(
+                            color = Color(0xFF1F2937),
+                            shape = RoundedCornerShape(size = 28797.12109.toPxDp)
+                        )
+                        .padding(
+                            start = 17.27988.toPxDp,
+                            top = 17.28008.toPxDp,
+                            end = 17.28012.toPxDp,
+                            bottom = 17.27992.toPxDp
+                        )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icon_palette),
+                        contentDescription = "image description",
+                        contentScale = ContentScale.None
+                    )
+                }
+            }
+        }
+        Column(
+            Modifier
+                .width(987.84003.toPxDp),
+            verticalArrangement = Arrangement.spacedBy(34.560001373291016.toPxDp, Alignment.Top),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Row(
+                Modifier
+                    .width(987.84003.toPxDp)
+                    .height(56.toPxDp)
+                    .padding(start = 408.96002.toPxDp, end = 408.96002.toPxDp),
+                horizontalArrangement = Arrangement.spacedBy(
+                    23.040000915527344.toPxDp,
+                    Alignment.CenterHorizontally
+                ),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "#3366FF",
+                    style = TextStyle(
+                        fontSize = 40.32.toPxSp,
+                        lineHeight = 56.45.toPxSp,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFFF2F2F2),
+                        textAlign = TextAlign.Center,
+                    ),
+                    modifier = Modifier
+                        .width(170.toPxDp)
+                        .height(56.toPxDp)
+                )
+            }
+            Text(
+                text = "@colorhunter",
+                style = TextStyle(
+                    fontSize = 51.84.toPxSp,
+                    lineHeight = 72.58.toPxSp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFF2F2F2),
+
+                    textAlign = TextAlign.Center,
+                ),
+                modifier = Modifier
+                    .width(987.84003.toPxDp)
+                    .height(80.64.toPxDp)
+            )
+        }
+    }
+}
+
 @Composable
 fun ProfileStatItem(cnt: Int, title: String){
     Column(
@@ -224,7 +357,7 @@ fun ProfileStatItem(cnt: Int, title: String){
                 cnt < 1000 -> cnt.toString()
                 cnt < 1000000 -> {
                     val k = cnt / 1000f
-                    if (k >= 100) "${k.toInt()}K" // 100K 이상은 소수점 생략이 깔끔함
+                    if (k >= 100) "${k.toInt()}K" // 100K 이상은 소수점 생략
                     else String.format("%.1fK", k).replace(".0K", "K")
                 }
 
@@ -237,10 +370,8 @@ fun ProfileStatItem(cnt: Int, title: String){
             style = TextStyle(
                 fontSize = 40.32.toPxSp,
                 lineHeight = 56.45.toPxSp,
-    //                           fontFamily = FontFamily(Font(R.font.inter)),
                 fontWeight = FontWeight(400),
                 color = Color(0xFFF2F2F2),
-
                 textAlign = TextAlign.Center,
             ),
             modifier = Modifier
@@ -251,15 +382,20 @@ fun ProfileStatItem(cnt: Int, title: String){
             text = title,
             style = TextStyle(
                 fontSize = 40.32.toPxSp,
-                //                         fontFamily = FontFamily(Font(R.font.inter)),
                 fontWeight = FontWeight(400),
                 color = Color(0xFFEEEEEE),
-
                 textAlign = TextAlign.Center,
             )
         )
     }
 }
+
+@Preview(device = Devices.PIXEL_4A)
+@Composable
+fun ProfileHeaderPreview(){
+    ProfileHeader()
+}
+
 @Preview(device = Devices.PIXEL_4A)
 @Composable
 fun ProfilePreview(){
