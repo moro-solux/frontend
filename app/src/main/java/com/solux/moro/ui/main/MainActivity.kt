25a57165972e718dee2ec1b93b54ae3e577212fc
sssuhha <ysh83@sookmyung.ac.kr>
2026-01-09 01:04:26 +0900

    chore: 불필요한 코드 주석 처리

--- app/src/main/java/com/solux/moro/ui/main/MainActivity.kt
+++ app/src/main/java/com/solux/moro/ui/main/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.solux.moro.core.designsystem.theme.MoroTheme
+import com.solux.moro.core.navigation.NavGraph
 import com.solux.moro.ui.map.MapScreen
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,18 +21,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MoroTheme {
-                MapScreen(
-                    posts = emptyList(),
-                    selectedPost = null,
-                    keyword = "",
-                    onKeywordChange = {},
-                    onSearch = {},
-                    onSelectPost = {},
-                    onClearSelection = {},
-                    onLoadNearby = { _, _ -> },
-                )
-//                val navController = rememberNavController()
-//                NavGraph(navController = navController)
+//                MapScreen(
+//                    posts = emptyList(),
+//                    selectedPost = null,
+//                    keyword = "",
+//                    onKeywordChange = {},
+//                    onSearch = {},
+//                    onSelectPost = {},
+//                    onClearSelection = {},
+//                    onLoadNearby = { _, _ -> },
+//                    hasFineLocationPermission = false,
+//                    lastKnownLatLng = null,
+//                    onLocationPermissionChanged = {},
+//                    onUpdateLastKnownLocation = {},
+//                )
+                val navController = rememberNavController()
+                NavGraph(navController = navController)
             }
         }
     }
